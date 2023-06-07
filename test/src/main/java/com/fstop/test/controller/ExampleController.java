e9bb04b34fa7d92e8561615e8f98699a2df352cd
Jay <famousoneman@gmail.com>
2023-06-07 13:21:07 +0800

    fix

--- test/src/main/java/com/fstop/test/controller/ExampleController.java
+++ test/src/main/java/com/fstop/test/controller/ExampleController.java
@@ -2,22 +2,15 @@ package com.fstop.test.controller;
 
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.fstop.test.entity.test;
 import com.fstop.test.service.exampleService;
-
-//import com.fstop.test.service.exampleService;
 
 import lombok.extern.slf4j.Slf4j;
 
